0a87ba6351459d13e9646fbceddfcb0677e01f81
Martinn <martinhruska770@gmail.comgit config --global user.email martinhruska770@gmail.com>
2023-09-25 11:39:50 +0200

    d

---
+++ python.py
@@ -0,0 +1,10 @@
+keys = ["John", "Marie", "Alex"];
+
+values = ["41", "15", "66"];
+
+slovnik = dict(zip(keys, values));
+
+print(slovnik);
+
+
+
---
+++ python2.py
@@ -0,0 +1,7 @@
+trida = {}
+trida["student.1"] = {"jméno": "Michaela", "věk": 19}
+trida["student.2"] = {"jméno": "Jakub", "věk": 18}
+trida["student.3"] = {"jméno": "Karolína", "věk": 20}
+trida["student.4"] = {"jméno": "David", "věk": 17}
+trida["student.5"] = {"jméno": "Tereza", "věk": 21}
+print(trida)
